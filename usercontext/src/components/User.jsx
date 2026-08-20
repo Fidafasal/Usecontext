fb2d5ed34903479d432528e898621b0519db014c
Fidafasal <fidafasal787@gmail.com>
2026-08-20 10:17:37 +0530

    updated

--- usercontext/src/components/User.jsx
+++ usercontext/src/components/User.jsx
@@ -1,17 +1,19 @@
 import { useState } from "react"
 import { useParams } from "react-router-dom";
 
-const User = () =>{
- 
- const { id } = useParams();
- console.log(id);
- 
- 
+const User = () => {
+
+    const { id } = useParams();
+    console.log("the id =", id);
+    console.log(useParams());
+
+
+
 
     return (
         <div>
             <h1>User page</h1>
-          <h2>User ID: {id}</h2>
+            <h2>User ID: {id}</h2>
         </div>
     )
 }
